353c1e651f0c9904f84043ac26f3b99827c3d608
Leonardo Ferraz Novaes <leonardo.ferrazn@gmail.com>
2022-09-29 10:19:30 -0300

    feat: teste unitario da camada model das funções getAll e getById

---
+++ tests/unit/models/productModel.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const { describe } = require('mocha');
+const sinon = require('sinon');
+const mocks = require('../../mock');
+
+const connection = require("../../../models/connection");
+const productsModel = require("../../../models/productsModel");
+
+describe("Testa a productModel", () => {
+  describe("verifica se retorna todo os produtos", () => {
+
+    it("Testa getAll", async () => {
+      sinon.stub(connection, "execute").resolves(mocks.allProductsResponse);
+      const result = await productsModel.getAll();
+      expect(result).to.deep.equal(mocks.allProductsResponse);
+    })
+
+    it("testa getById", async () => {
+      sinon.stub(connection, "execute").resolves(mocks.productSearchIdResponse)
+      const result = await productsModel.getById();
+      expect(result).to.deep.equal(mocks.productSearchIdResponse);
+    })
+
+  });
+  
+  afterEach(sinon.restore);
+})
