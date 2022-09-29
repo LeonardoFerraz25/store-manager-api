cc329249b8883ac7942f6eee17d146321ba05366
Leonardo Ferraz Novaes <leonardo.ferrazn@gmail.com>
2022-09-29 11:14:15 -0300

    feat: add pasta de rotas e rotas de produtos GET para todo e GET pelo id

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const routes = require('./routes');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/products', routes.productRouter);
 
 module.exports = app;
