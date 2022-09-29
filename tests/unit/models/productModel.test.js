26e9427f2ed353c37f222d646e3ef5d8045e49e7
Leonardo Ferraz Novaes <leonardo.ferrazn@gmail.com>
2022-09-29 19:15:45 -0300

    feat(tests): add testes das funções create, update e remove da camada models de produtos

--- tests/unit/models/productModel.test.js
+++ tests/unit/models/productModel.test.js
@@ -7,9 +7,9 @@ const connection = require("../../../src/models/connection");
 const productsModel = require("../../../src/models/productModel");
 
 describe("Testa a productModel", () => {
-  describe("verifica se retorna todo os produtos", () => {
+  describe("verifica funções da camada model dos produtos", () => {
 
-    it("Testa getAll", async () => {
+    it("Testa 'getAll'", async () => {
       const allProducts = [[...mocks.allProductsResponse]];
       sinon.stub(connection, "execute").resolves(allProducts);
       const result = await productsModel.getAll();
@@ -17,12 +17,36 @@ describe("Testa a productModel", () => {
       expect(prds).to.deep.equal(allProducts);
     })
 
-    it("testa getById", async () => {
+    it("testa 'getById'", async () => {
       const product = mocks.productSearchIdResponse;
       sinon.stub(connection, "execute").resolves(product)
       const result = await productsModel.getById(1);
       const prd = [result];
       expect(prd).to.deep.equal(product);
+    })
+
+    it("testa 'create'", async () => {
+      const product = [{insertId: 4}];
+      sinon.stub(connection, "execute").resolves(product);
+      const name = 'Produto1';
+      const result = await productsModel.create(name);
+      expect(result).to.deep.equal(4);
+    })
+
+    it("testa 'update'", async () =>  {
+      const product = [{affectedRows: 1}];
+      sinon.stub(connection, "execute").resolves(product);
+      const id = 4;
+      const result = await productsModel.update(id);
+      expect(result).to.deep.equal(1);
+    })
+
+    it("testa 'remove'", async () => {
+      const product = [{affectedRows: 1}];
+      sinon.stub(connection, "execute").resolves(product);
+      const id = 4;
+      const result = await productsModel.remove(id);
+      expect(result).to.deep.equal(1);
     })
 
   });
