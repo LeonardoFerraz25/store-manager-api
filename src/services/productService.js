e633f032030c4dd6cb742e154f46187000eeb1c5
Leonardo Ferraz Novaes <leonardo.ferrazn@gmail.com>
2022-09-29 10:47:50 -0300

    feat: add camada de service e com as funções getAll e getByid

---
+++ src/services/productService.js
@@ -0,0 +1,16 @@
+const productModel = require('../models/productModel');
+
+const getAll = async () => {
+  const products = await productModel.getAll();
+  return products;
+};
+
+const getById = async (id) => {
+  const product = await productModel.getById(id);
+  return product;
+}
+
+module.exports = {
+  getAll,
+  getById
+}
