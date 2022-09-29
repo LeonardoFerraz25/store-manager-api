02f9d05b9051a3f1f620f1314bf91618328da9b9
Leonardo Ferraz Novaes <leonardo.ferrazn@gmail.com>
2022-09-29 13:31:01 -0300

    feat: add função create na model de produtos

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -6,11 +6,17 @@ const getAll = async () => {
 }
 
 const getById = async (id) => {
-  const [product] = await connection.execute('SELECT * FROM StoreManager.products WHERE id = ?', [id])
+  const [product] = await connection.execute('SELECT * FROM StoreManager.products WHERE id = ?', [id]);
   return product;
+}
+
+const create = async (name) => {
+  const [product] = await connection.execute('INSERT INTO (name) VALUES (?)', [name]);
+  return { id: product.insertId };
 }
 
 module.exports = {
   getAll,
-  getById
+  getById,
+  create
 }
